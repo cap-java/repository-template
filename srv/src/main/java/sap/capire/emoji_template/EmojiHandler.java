4da5ca9ca70912c63c0bcbd04b62e1b3979328a2
Marvin Lindner <marvin.lindner@sap.com>
2025-11-10 13:27:27 +0100

    version bump and javadoc

--- srv/src/main/java/sap/capire/emoji_template/EmojiHandler.java
+++ srv/src/main/java/sap/capire/emoji_template/EmojiHandler.java
@@ -1,9 +1,5 @@
 package sap.capire.emoji_template;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import com.sap.cds.Result;
 import com.sap.cds.reflect.CdsElement;
 import com.sap.cds.reflect.CdsStructuredType;
@@ -13,31 +9,36 @@ import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+/**
+ * Event handler that decorates fields annotated with @emoji by appending an emoji.
+ */
 @ServiceName(value = "*", type = ApplicationService.class)
 public class EmojiHandler implements EventHandler {
-	
 	private static final String EMOJI_ANNOTATION_NAME = "@emoji";
 
 	@After(event = CqnService.EVENT_READ)
-	public void decorateEmoji(CdsReadEventContext ctx) {
+	public void decorateEmoji(final CdsReadEventContext ctx) {
 
 		ctx.getResult().list().forEach(row -> {
 			Set<String> emojiAnnotatedElments = checkForEmojiAnnotatedElments(ctx.getResult());
 			row.keySet().forEach(key -> {
-				if(emojiAnnotatedElments.contains(key)) {
+				if (emojiAnnotatedElments.contains(key)) {
 					row.put(key, row.get(key) + " 🙃");
 				}
 			});
-		}
-		);
+		});
 	}
 
-	private Set<String> checkForEmojiAnnotatedElments(Result result) {
+	private Set<String> checkForEmojiAnnotatedElments(final Result result) {
 		Set<String> annotatedElementNames = new HashSet<>();
-		CdsStructuredType rowType = result.rowType();//.annotations().anyMatch(anno -> anno.getName().equals("@emoji"))) 
-		for(CdsElement element : rowType.elements().collect(Collectors.toList())) {
-			if(element.findAnnotation(EMOJI_ANNOTATION_NAME).isPresent()) {
+		CdsStructuredType rowType = result.rowType();// .annotations().anyMatch(anno ->
+														// anno.getName().equals("@emoji")))
+		for (CdsElement element : rowType.elements().collect(Collectors.toList())) {
+			if (element.findAnnotation(EMOJI_ANNOTATION_NAME).isPresent()) {
 				annotatedElementNames.add(element.getName());
 			}
 		}
